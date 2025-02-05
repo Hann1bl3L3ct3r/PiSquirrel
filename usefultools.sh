a196ffc163710fc3c2e05a17235cd691924d0fba
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-02-05 15:01:36 -0500

    Update usefultools.sh

--- usefultools.sh
+++ usefultools.sh
@@ -3,13 +3,17 @@ mkdir Tools
 cd Tools
 
 # Install python, ruby, and nmap directly 
-sudo apt install python3-pip nmap python3-dev tcpdump
+sudo apt install python3-pip nmap python3-dev tcpdump pipx
 
 # Install impacket framework (this is why we made sure to add bin to path previously) 
 pip3 install impacket 
+pip3 install certipy-ad 
 
 # Pull down an assortment of tools including Responder, mitm6, ASRepCatcher, and the Bloodhound.py ingester
 git clone https://github.com/lgandx/Responder.git
 git clone https://github.com/dirkjanm/mitm6.git
 git clone https://github.com/Yaxxine7/ASRepCatcher.git
 git clone https://github.com/dirkjanm/BloodHound.py.git
+
+# Installing netexec package
+pipx install git+https://github.com/Pennyw0rth/NetExec
