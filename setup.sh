b28984febd1a31becfca8195aecb0e03ff883348
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-02-05 09:38:48 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@ sudo apt -y update
 sudo apt -y upgrade 
 sudo apt -y install bridge-utils nano  
 sudo hostnamectl set-hostname HPLASERJET
-echo -e "pi\npisquirrel\npisquirrel" | passwd   
 curl -s https://install.zerotier.com | sudo bash
 echo "PATH=$PATH:/home/pi/.local/bin" >> .bashrc
 sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/interfaces > /etc/network/interfaces
