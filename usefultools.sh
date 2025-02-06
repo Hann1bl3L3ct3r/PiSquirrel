4182c6f277b3e7dba7f45b4ee3cf018fd17040a9
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-02-06 16:50:24 -0500

    Update usefultools.sh

--- usefultools.sh
+++ usefultools.sh
@@ -2,11 +2,14 @@
 mkdir Tools
 cd Tools
 
+# Fix Python references through symlink
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
 # Install python, nmap, tcpdump, etc from apt  
 sudo apt install -y python3-pip nmap python3-dev tcpdump pipx bettercap
 
 # Install impacket framework, certipy-ad, etc from apt instead of pip (this is why we made sure to add bin to path previously) 
-sudo apt install -y python3-impacket python3-certipy
+pip install impacket certipy-ad --break-system-packages
 
 # Pull down an assortment of tools including Responder, mitm6, ASRepCatcher, and the Bloodhound.py ingester
 git clone https://github.com/lgandx/Responder.git
