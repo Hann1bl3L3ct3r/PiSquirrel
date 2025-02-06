8f96f6296b9e6327fd9ac712b30b8f36ea3e0c0b
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-02-06 15:51:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ sudo hostnamectl set-hostname HPLASERJET
 curl -s https://install.zerotier.com | sudo bash
 echo "PATH=$PATH:/home/pi/.local/bin" >> .bashrc
 sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/interfaces > /etc/network/interfaces
+sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/hosts > /etc/hosts
 sudo systemctl restart networking 
