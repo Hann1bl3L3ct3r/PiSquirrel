b9ec5d0a572be5f75624d5f31fdd8683e5765aba
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-02-14 11:37:15 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt -y update 
 sudo apt -y upgrade 
 sudo apt -y install bridge-utils nano  
-sudo hostnamectl set-hostname HPLASERJET
+sudo hostnamectl set-hostname BROTHERHL-L2315D
 curl -s https://install.zerotier.com | sudo bash
 echo "PATH=$PATH:/home/pi/.local/bin" >> .bashrc
 sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/interfaces > /etc/network/interfaces
