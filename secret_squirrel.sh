701d60101800c5774ba5e9ac6bbe17cb75134c06
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-12-10 11:44:35 -0500

    Update secret_squirrel.sh
    Added install and setup for lighttpd and fake Brother printer web interface on TCP 8080.

--- secret_squirrel.sh
+++ secret_squirrel.sh
@@ -9,6 +9,13 @@ sudo hostnamectl set-hostname BROTHERHL-L2315D || die "Failed to change hostname
 echo "[INFO] Downloading hosts configuration..."
 sudo curl -fsSL https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/hosts -o /etc/hosts || die "Failed to fetch hosts file."
 
+# Installing and configuring 
+echo "[INFO] Installing lighttpd for fake web interface on TCP 8080"
+sudo apt -y install lighttpd
+sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/lighttpd.conf > /etc/lighttpd/lighttpd.conf 
+sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/index.html > /var/www/html/index.html 
+sudo systemctl restart lighttpd
+
 # Install SNMP sim tool to fake replies using real recorded SNMP data 
 echo "[INFO] Setting up fake SNMP server..."
 sudo pip install snmpsim --break-system-packages
@@ -42,4 +49,5 @@ su pi
 # Setup crontab to automate fake port scripts starting on reboot for pi  
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/pi_cronjobs > /home/pi/Tools/FakePrinter/pi_cronjobs
 crontab /home/pi/Tools/FakePrinter/pi_cronjobs
+
 
