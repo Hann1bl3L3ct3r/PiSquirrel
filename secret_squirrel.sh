7ea3adc6cfa19eb7270bca9834b9ba0abc424608
Hann1bl3L3ct3r <141247795+Hann1bl3L3ct3r@users.noreply.github.com>
2025-12-10 11:20:03 -0500

    Update secret_squirrel.sh
    Moved hostname changes from setup script, added process output.

--- secret_squirrel.sh
+++ secret_squirrel.sh
@@ -1,13 +1,22 @@
 # Fake being a printer for stealth on the network 
 # ONLY RUN THIS POST TOOL SETUP
 
+# Change system hostname to that of a Brother 
+echo "[INFO] Setting hostname..."
+sudo hostnamectl set-hostname BROTHERHL-L2315D || die "Failed to change hostname."
+
+echo "[INFO] Downloading hosts configuration..."
+sudo curl -fsSL https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/hosts -o /etc/hosts || die "Failed to fetch hosts file."
+
 # Install SNMP sim tool to fake replies using real recorded SNMP data 
+echo "[INFO] Setting up fake SNMP server..."
 sudo pip install snmpsim --break-system-packages
 mkdir /home/pi/Tools/FakePrinter
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/brother.snmprec > /home/pi/Tools/FakePrinter/brother.snmprec
 sudo setcap 'cap_net_bind_service=+ep' $(which python3.11)
 
 # Install FTP server and start server process 
+echo "[INFO] Setting up fake FTP server..."
 sudo apt -y install vsftpd
 mkdir /home/pi/Tools/FakePrinter/FakeFTP
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/CFG-PAGE.TXT > /home/pi/Tools/FakePrinter/FakeFTP/CFG-PAGE.TXT
@@ -15,15 +24,18 @@ sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads
 sudo systemctl enable --now vsftpd
 
 # Pull down and save python scripts to fake specific open printer ports
+echo "[INFO] Pulling down scripts to fake print ports..."
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/fake_9100.py > /home/pi/Tools/FakePrinter/fake_9100.py
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/fake_515.py > /home/pi/Tools/FakePrinter/fake_515.py
 curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/fake_631.py > /home/pi/Tools/FakePrinter/fake_631.py
 
 # Setup crontab to automate fake port scripts starting on reboot 
+echo "[INFO] Setting up fake print ports cronjobs ..."
 sudo curl https://raw.githubusercontent.com/Hann1bl3L3ct3r/PiSquirrel/refs/heads/main/FakePrinterFiles/cronjobs > /home/pi/Tools/FakePrinter/cronjobs
 sudo crontab /home/pi/Tools/FakePrinter/cronjobs 
 
 # Change users
+echo "[INFO] Setting up fake SNMP server cronjobs..."
 su pi
 
 # Setup crontab to automate fake port scripts starting on reboot for pi  
